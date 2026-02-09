336108907329684a297fd5075146837092b17707
Siddu Ramagiri <sidduramagiri3@gmail.com>
2026-02-09 23:28:43 +0530

    deploy fix

--- app.py
+++ app.py
@@ -181,5 +181,6 @@ def uploaded_file(filename):
 # ----------------------------------------------------------
 # 8️⃣ Run
 # ----------------------------------------------------------
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
